e2c775163e72321944da5591111bd911bfb8ef57
Renobeet <153073996+Renobeet@users.noreply.github.com>
2026-02-11 08:56:31 -0800

    Remove background image and related functions
    Removed unused background image loading and related functions.

--- game/sketch.js
+++ game/sketch.js
@@ -1,20 +1,3 @@
-let bg;
-
-function preload() {
-  // 注意：从 game/ 里访问 assets/，要返回上一层
-  bg = loadImage('../assets/Background.PNG');
-}
-
-function setup() {
-  createCanvas(900, 700);
-}
-
-function draw() {
-  background(220);
-  if (bg) image(bg, 0, 0, width, height);
-}
-
-
 
 
 // ================================
